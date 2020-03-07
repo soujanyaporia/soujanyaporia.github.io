312e98950f472e345ed76c84ab982d6d7fdd8a7e
Navonil Majumder <n.majumder.2009@gmail.com>
2020-03-07 00:47:52 -0600

    Removed masthead and other logo with its toggle

--- assets/js/skinToggle.js
+++ assets/js/skinToggle.js
@@ -8,8 +8,6 @@
 // }
 
 function disableDarkMode() {
-    var link = document.getElementById('nav-bar-logo');
-    link.src =  link.src.replace('dark', 'light');
     link = document.getElementById('dark-toggle-link');
     link.href =  link.href.replace('dark', 'light');
     var e = document.getElementById('dark-toggle');
@@ -17,15 +15,10 @@ function disableDarkMode() {
         e.classList.add('fa-toggle-off');
         if (e !== null)
             e.classList.remove('fa-toggle-on');
-    e = document.getElementById('big-home-logo');
-    if (e !== null)
-        e.src = e.src.replace('dark', 'light');
     localStorage.setItem("darkMode", null);
 }
 
 function enableDarkMode() {
-    var link = document.getElementById('nav-bar-logo');
-    link.src =  link.src.replace('light', 'dark');
     link = document.getElementById('dark-toggle-link');
     link.href =  link.href.replace('light', 'dark');
     var e = document.getElementById('dark-toggle');
@@ -33,9 +26,6 @@ function enableDarkMode() {
         e.classList.add('fa-toggle-on');
         if (e !== null)
             e.classList.remove('fa-toggle-off');
-    e = document.getElementById('big-home-logo');
-    if (e !== null)
-        e.src = e.src.replace('light', 'dark');
     localStorage.setItem("darkMode", "enabled");
 }
 
