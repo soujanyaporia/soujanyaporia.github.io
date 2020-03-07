b4b01f598dae03c9b0903663b3c3c6c6f8c3fcca
Navonil Majumder <n.majumder.2009@gmail.com>
2020-03-07 04:28:31 -0600

    Navigaition bar fixed

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -8,7 +8,6 @@ $(window).on("load", function() {
   var $btn = $("nav.greedy-nav .greedy-nav__toggle");
   var $vlinks = $("nav.greedy-nav .visible-links");
   var $hlinks = $("nav.greedy-nav .hidden-links");
-  var $sitelogo = $("#nav-bar-logo");
 
   var numOfItems = 0;
   var totalSpace = 0;
@@ -26,7 +25,7 @@ $(window).on("load", function() {
 
   function check() {
     // Get instant state
-    availableSpace = $vlinks.width() - $btn.width() - $sitelogo.width();
+    availableSpace = $vlinks.width() - $btn.width();
     numOfVisibleItems = $vlinks.children().length;
     requiredSpace = breakWidths[numOfVisibleItems - 1];
 
